6aee44d4d83cb5e024fa27d200b494545bb7f2db
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-11-04 10:22:21 -0300

    feat: change to gradle and improving Clean Arc

--- src/main/java/br/com/fiap/mottu/dto/MotoDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.mottu.dto;
-
-public record MotoDTO(Long id, String modelo, String placa, String status, PatioWithoutMotoDTO patio,java.util.Date dataEntrada) {
-
-}
--- src/main/java/br/com/fiap/mottu/dto/PatioDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.mottu.dto;
-
-public record PatioDTO(Long id, String nome, String endereco, String complemento, java.math.BigDecimal areaM2, Long idLocalidade, java.util.List<MotoDTO> motos) {
-
-}
